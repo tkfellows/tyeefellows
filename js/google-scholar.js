daa400e6da1a8cad261679fc314d2be98851a6a4
Tyee Fellows <105998002+tkfellows@users.noreply.github.com>
2022-05-28 13:54:56 -0400

    Update google-scholar.js

--- js/google-scholar.js
+++ js/google-scholar.js
@@ -1,23 +1,5 @@
-// const GOOGLE_SCHOLAR_API_URL = 'https://serpapi.com/search.json?engine=google_scholar_author';
-// const AUTHOR_ID = 'sbkVZRsAAAAJ';
-// const SECRETE_API_KEY = '2288f953bd7078146df9eea93be7081fd5c065a5b81fbca16239805b7ff5b78b';
-
-// async function getGoogleScholarData() {
-//     // const response = await fetch(GOOGLE_SCHOLAR_API_URL + '&author_id=' + AUTHOR_ID + '&api_key=' + SECRETE_API_KEY);
-//     const response = await fetch("https://serpapi.com/searches/9a4fb7c3ff88425b/629247c4ea1442bb52735048.json");
-//     const gsdata = response.json();
-//     console.log(gsdata);
-// }
-
-// document.addEventListener('DOMContentLoaded', function() {
-//     getGoogleScholarData();
-// });
-
-var dd = document.querySelector('dd');
-
 var xhr = new XMLHttpRequest();
-
-xhr.open('GET','https://cors-anywhere.herokuapp.com/https://scholar.google.com/citations?user=sbkVZRsAAAAJ&hl=en');
+xhr.open('GET','https://scholar.google.com/citations?user=sbkVZRsAAAAJ&hl=en');
 xhr.responseType = 'document';
 
 xhr.onload = function() {
