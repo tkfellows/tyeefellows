f39f608c4dc91f6d22a046f56e55ae41bf81b68e
Tyee Fellows <105998002+tkfellows@users.noreply.github.com>
2022-06-18 13:58:23 -0400

    Add files via upload

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,50 +1,120 @@
+function setAttributes(element, attributes) {
+    Object.keys(attributes).forEach(attr => {
+      element.setAttribute(attr, attributes[attr]);
+    });
+  }
+  
 function getNavbarHeader() {
-    let navbarHeader = document.getElementById("navbar-header");
-    navbarHeader.innerHTML=
+    let navbarDiv = document.getElementById("navbar-header");
+
+    var navBar = document.createElement("nav");
+    const navBarAttributes = {
+        class: "navbar navbar-expand-lg navbar-light",
+        style: "background-color: #e3f2fd;"
+    };
+    setAttributes(navBar, navBarAttributes);
+
+    var navBarContainer = document.createElement("div");
+    navBarContainer.setAttribute("class","container-lg");
+
+    var navBarMainLogo = document.createElement("a");
+    const navBarMainLogoAttributes = {
+        class: "navbar-brand text-black fs-3",
+        href: "http://tyeefellows.com/index.html"
+    };
+    setAttributes(navBarMainLogo, navBarMainLogoAttributes);
+    navBarMainLogo.innerHTML = `
+        <svg
+            width="64"
+            height="64"
+            viewBox="0 0 12 11"
+            xmlns="http://www.w3.org/2000/svg">
+            <g
+                id="heart-ecg-group"
+                transform="matrix(0.58370756,0,0,0.66147612,-0.40626343,5.3648453)"
+                >
+                <path
+                    id="heart"
+                    d="m 4.1778986,1.6821692 c 0.6819226,0.9054484 1.1786566,1.4344211 2.2410591,2.3139001 m 2.5338434,1.874943 C 9.2853783,6.1079416 9.5196663,6.2755009 9.8756648,6.510807 12.624224,4.6940776 14.200775,3.3638094 15.445969,1.7515195 m 1.302875,-1.89974509 0.354535,-0.60826304 c 3.256574,-6.22551197 -3.691603,-9.47803437 -7.05456,-6.38103937 -0.059883,0.05351 -0.1176452,0.1093471 -0.1731542,0.167382 -0.05615,-0.05812 -0.115974,-0.113204 -0.175077,-0.167382 -0.84074,-0.774002 -1.89219,-1.12916 -2.9455311,-1.127421 -3.160024,0.0052 -6.44571315,3.0129734 -4.0040212,7.68210736 l 0.1661853,0.30483645"
+                    style="opacity:1;fill:none;fill-opacity:1;fill-rule:nonzero;stroke:#00000f;stroke-width:0.746;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+                    transform="translate(-0.05084201,0.58961957)" 
+                    />
+                <path
+                    id="ecg" 
+                    style="fill:none;fill-opacity:1;stroke:#d2000f;stroke-width:0.480964;stroke-linecap:round;stroke-linejoin:round;stroke-dasharray:none;stroke-opacity:1"
+                    d="m 0.93670493,1.3232841 0.37438177,-0.015055 0.65392,-0.020073 0.7088294,-0.020073 0.4742168,-0.020073 0.4592417,-0.010033 0.3044971,0.015055 0.3594064,0.095348 0.2795384,0.095348 0.2246291,0.1555666 0.324464,0.2408773 0.329456,0.1856763 0.2595713,0.1154207 0.3094889,0.070252 0.2246289,-0.010037 0.104827,-0.125457 0.069885,-0.3562978 0.049918,-0.4064804 0.069885,-0.43659024 0.024959,-0.63732121 0.024958,-0.81296099 0.099835,-1.04380176 0.109818,-1.1993685 0.1846949,-1.4753735 0.1996703,-2.0625122 0.1801395,2.9677218 0.023782,0.812961 0.015704,1.2777412 v 1.37500806 l -0.005,1.20438664 -0.014976,1.6309403 V 3.9779531 L 7.3910457,5.2375408 7.4359716,6.2662874 7.6456253,8.2786177 7.9900564,6.2211232 8.039974,5.2977601 8.0948834,4.3643604 8.204702,3.5865275 8.3195124,3.0596082 8.4193475,2.7735665 8.5890672,2.4875246 8.7587869,2.2667204 8.9734325,2.0559527 9.1780944,1.9104226 9.4077152,1.7849657 9.6922454,1.7096915 10.036677,1.664527 l 0.289521,-0.065238 0.284531,-0.1104022 0.264563,-0.1455299 0.22962,-0.2358592 0.149753,-0.21076763 0.25458,-0.50182777 0.239604,-0.42655355 0.264563,-0.45164499 0.239604,-0.45666334 0.199671,-0.41651702 0.124793,-0.3412429 0.174712,-0.3261881 0.214645,-0.1605849 0.08985,-0.065237 0.114811,0.015055 0.219637,0.2057495 0.204663,0.3312063 0.229621,0.4666998 0.184694,0.4767364 0.184696,0.4354493 0.144761,0.38754825 0.164727,0.36633421 0.194679,0.25091389 0.174712,0.19069455 0.19967,0.1957129 0.239605,0.1605849 0.184694,0.065237 0.244596,0.055201 0.364398,0.035128 0.439274,0.025091 0.504169,0.030109 0.434282,-0.00502 0.354415,-0.020073 0.494184,-0.055201 0.534118,-0.045164 0.628961,-0.025091 0.648929,0.020073 0.454248,-0.00502 0.349424,-0.00502 h 0.06489"
+                    transform="translate(0,0.1)" 
+                    />
+                <circle 
+                    id="beat"
+                    style="fill:#d2000f"
+                    cx="0.214rem" 
+                    cy="0.084rem" 
+                    r="0.48"/>
+            </g>
+        </svg>
+        `
+    
+    var navBarButton = document.createElement("button");
+    const navBarButtonAttributes = {
+        "class": "navbar-toggler",
+        "type": "button",
+        "data-bs-toggle": "collapse",
+        "data-bs-target": "#navbarSupportedContent",
+        "aria-controls": "navbarSupportedContent",
+        "aria-expanded": "false",
+        "aria-label": "Toggle navigation"        
+    }
+    setAttributes(navBarButton,navBarButtonAttributes);
+    var navBarSpan = document.createElement("span");
+    navBarSpan.setAttribute("class","navbar-toggler-icon");
+    navBarButton.appendChild(navBarSpan);
+
+    var navBarContent = document.createElement("div");
+    const navBarContentAttributes = {
+        class: "collapse navbar-collapse",
+        id: "navbarSupportedContent"
+    }
+    setAttributes(navBarContent, navBarContentAttributes);
+    navBarContent.innerHTML = 
     `
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-        <div class="container-lg">
-            <a class="navbar-brand text-black fs-3" href="http://tyeefellows.com/index.html">
-                <i class="bi bi-heart-pulse me-2" style='font-size:50px;'></i>
-            </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        About
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="http://tyeefellows.com/about-me.html">About me</a></li>
-                        <li><a class="dropdown-item" href="http://tyeefellows.com/documents/Fellows_CV.pdf" download="Fellows_CV.pdf">CV</a></li>
-                    </ul>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="http://tyeefellows.com/research.html">Research</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="http://tyeefellows.com/publications.html">Publications</a>
-                    </li>
-                    <li class="nav-item">
-                    <a class="nav-link" href="http://tyeefellows.com/media.html">Media</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Hobbies
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/coding-projects.html">Coding</a></li>
-                        <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/climbing.html">Climbing</a></li>
-                        <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/photography.html">Photography</a></li>
-                    </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            About
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="http://tyeefellows.com/about-me.html">About me</a></li>
+            <li><a class="dropdown-item" href="http://tyeefellows.com/documents/Fellows_CV.pdf" download="Fellows_CV.pdf">CV</a></li>
+        </ul>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="http://tyeefellows.com/research.html">Research</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="http://tyeefellows.com/publications.html">Publications</a>
+        </li>
+        <li class="nav-item">
+        <a class="nav-link" href="http://tyeefellows.com/media.html">Media</a>
+        </li>
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Hobbies
+        </a>
+        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/coding-projects.html">Coding</a></li>
+            <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/climbing.html">Climbing</a></li>
+            <li><a class="dropdown-item" href="http://tyeefellows.com/hobbies/photography.html">Photography</a></li>
+        </ul>
+        </li>
+    </ul>
     `;
+
+    navBarContainer.appendChild(navBarMainLogo);
+    navBarContainer.appendChild(navBarButton);
+    navBarContainer.appendChild(navBarContent);
+    navBar.appendChild(navBarContainer);
+    navbarDiv.appendChild(navBar);
 };
 getNavbarHeader();
 
---
+++ js/main-logo-animation.js
@@ -0,0 +1,28 @@
+gsap.registerPlugin(MotionPathPlugin);
+logoAnimation = gsap.to("#beat", 
+    {
+        x:10,
+        y:10,
+        duration: 1,
+        paused: true,
+        ease: "power4",
+        repeat: -1,
+        scale:2,
+        motionPath: {
+            path: "#ecg",
+            align: "#ecg",
+            alignOrigin: [0.5, 0.5],
+            autoRotate: true,
+            start: -0.035,
+            end:0.995,
+        }
+    }
+    );
+$("svg").hover(function() {
+        logoAnimation.play();
+        logoAnimation.eventCallback("onRepeat", function(){});
+    }, function() {
+        logoAnimation.eventCallback("onRepeat", function() { 
+            logoAnimation.pause();
+        });
+    });
