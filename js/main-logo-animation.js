1440d548b5f889209a724b4658108c474e7df349
Tyee Fellows <105998002+tkfellows@users.noreply.github.com>
2022-06-19 13:02:23 -0400

    Add files via upload

--- js/main-logo-animation.js
+++ js/main-logo-animation.js
@@ -1,4 +1,5 @@
 gsap.registerPlugin(MotionPathPlugin);
+
 logoAnimation = gsap.to("#beat", 
     {
         x:10,
@@ -7,22 +8,34 @@ logoAnimation = gsap.to("#beat",
         paused: true,
         ease: "power4",
         repeat: -1,
-        scale:2,
+        scale:1.5,
         motionPath: {
             path: "#ecg",
             align: "#ecg",
             alignOrigin: [0.5, 0.5],
             autoRotate: true,
             start: -0.035,
-            end:0.97,
+            end:0.96,
         }
-    }
-    );
+    });
+heartAnimation = gsap.to("#heart",
+    {
+        duration: 1,
+        paused: true,
+        ease: "power4",
+        repeat: -1,
+        scale: 1.03
+    });
 $("svg#main-logo").hover(function() {
         logoAnimation.play();
+        heartAnimation.play();
         logoAnimation.eventCallback("onRepeat", function(){});
+        heartAnimation.eventCallback("onRepeat", function(){});
     }, function() {
         logoAnimation.eventCallback("onRepeat", function() { 
             logoAnimation.pause();
+        });
+        heartAnimation.eventCallback("onRepeat", function() { 
+            heartAnimation.pause();
         });
     });
